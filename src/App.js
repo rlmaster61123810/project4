4bac2c5f27494ae423db010f0136dad8ac3f9ea5
Nontawat Wuttifaey <mistidea9@gmail.com>
2021-02-20 15:49:41 +0700

    sss

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Home from "./screen/home";
+import Login from "./screen/auth/login";
 function App() {
-  return (
-    <div>
-      <Router>
-        <Switch>
-          <Route path="/" component={Home} />
-        </Switch>
-      </Router>
-    </div>
-  );
+ return (
+   <div>
+     <Router>
+       <Switch>
+         <Route exact path="/" component={Home} />
+         <Route path="/login" component={Login} />
+       </Switch>
+     </Router>
+   </div>
+ );
 }
 
 export default App;
--- src/screen/home/index.js
+++ src/screen/home/index.js
@@ -1,8 +1,69 @@
+import { useState } from "react";
+import {
+  Collapse,
+  Container,
+  DropdownItem,
+  DropdownMenu,
+  DropdownToggle,
+  Jumbotron,
+  Nav,
+  Navbar,
+  NavbarBrand,
+  NavbarText,
+  NavbarToggler,
+  NavItem,
+  NavLink,
+  UncontrolledDropdown,
+} from "reactstrap";
+import Slider from "infinite-react-carousel";
+
 const Home = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggle = () => setIsOpen(!isOpen);
   return (
     <div>
-      <h1>Home Page</h1>
-      <button className="btn btn-danger">อะไรก็ได้</button>
+      <Container>
+        <Jumbotron>
+          <h1>Hello, world!</h1>
+        </Jumbotron>
+        <div>
+          <Navbar color="light" light expand="md">
+            <NavbarBrand href="/">wattano</NavbarBrand>
+            <NavbarToggler onClick={toggle} />
+            <Collapse isOpen={isOpen} navbar>
+              <Nav className="mr-auto" navbar>
+                <NavItem>
+                  <NavLink href="/components/">Components</NavLink>
+                </NavItem>
+                <NavItem></NavItem>
+              </Nav>
+              <NavbarText>Simple Text</NavbarText>
+            </Collapse>
+          </Navbar>
+          <Slider dots autoplay>
+            <div>
+              <img
+              src="https://www.cmru.ac.th/assets/images/banners/th/29-12-2020/355e723cf01db446a3ad5daa70225bb1901936d1.png"
+              alt="pic1"
+              />
+            </div>
+            <div>
+              <h3>2</h3>
+            </div>
+            <div>
+              <h3>3</h3>
+            </div>
+            <div>
+              <h3>4</h3>
+            </div>
+            <div>
+              <h3>5</h3>
+            </div>
+          </Slider>
+        </div>
+        <h1>Home Page</h1>
+      </Container>
     </div>
   );
 };
